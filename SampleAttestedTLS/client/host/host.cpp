5b0d2afaece49bfa094f2131536dce5eb14018ee
Zhang, Shunda <shunda.zhang@intel.com>
2024-05-13 08:33:43 +0800

    Move restful server to client.

--- SampleAttestedTLS/client/host/host.cpp
+++ SampleAttestedTLS/client/host/host.cpp
@@ -31,8 +31,73 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 
+#include <cpprest/http_listener.h>
+#include <cpprest/json.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+
 #define TLS_SERVER_NAME "localhost"
 #define TLS_SERVER_PORT "12340"
+
+//Restful Server
+
+using namespace web;
+using namespace web::http;
+using namespace web::http::experimental::listener;
+
+void handle_sql(http_request request) {
+    ucout << "Received POST request to /sql" << std::endl;
+
+    // Extract JSON data from the request
+    request.extract_json()
+    .then([&request](json::value data) {
+        // Get the SQL data from the JSON
+        utility::string_t sql_data = data[U("data")].as_string();
+
+        // Add the query "SHOW STATUS LIKE 'Ssl_cipher';" to SQL data
+        sql_data += "SHOW STATUS LIKE 'Ssl_cipher';";
+
+        // Generate a random filename
+        std::string filename = std::to_string(rand()) + ".sql";
+        std::string input_file = "/tmp/" + filename + ".in";
+        std::string output_file = "/tmp/" + filename + ".out";
+
+        std::string sql_client = "../SampleAttestedTLS/client/host/tls_client_host";
+        std::string sql_enclave = "../SampleAttestedTLS/client/enc/tls_client_enclave.signed.so";
+        std::string sql_server = "127.0.0.1";
+        std::string sql_port = "3307";
+
+        // Write SQL data to input file
+        std::ofstream input_stream(input_file);
+        size_t pos = 0;
+        while ((pos = sql_data.find(';', pos)) != std::string::npos) {
+            sql_data.insert(pos + 1, "\n");
+            ++pos;
+        }
+        input_stream << sql_data;
+        input_stream.close();
+
+        // Call client program
+        std::string command = sql_client + " " + sql_enclave + " -server:" + sql_server + " -port:" + sql_port + " -in:" + input_file + " -out:" + output_file;
+        system(command.c_str());
+
+        // Read client output
+        std::ifstream output_stream(output_file);
+        std::string output_content((std::istreambuf_iterator<char>(output_stream)), std::istreambuf_iterator<char>());
+        output_stream.close();
+
+        // Construct JSON response
+        json::value response;
+        response[U("output")] = json::value::string(U(output_content));
+
+        // Send the output content as response
+        request.reply(status_codes::OK, response);
+    })
+    .wait();
+}
+
 
 /* Global EID shared by multiple threads */
 sgx_enclave_id_t client_global_eid = 0;
@@ -172,6 +237,26 @@ int main(int argc, const char* argv[])
     char* server_port = NULL;
     char* input_file = NULL;
     char* output_file = NULL;
+
+    //Lauch Restful Server
+    srand(time(NULL));
+
+    http_listener listener(U("http://0.0.0.0:8088/sql"));
+    listener.support(methods::POST, handle_sql);
+
+    try {
+        listener.open().then([&listener]() {
+            ucout << "Listening for requests at: " << listener.uri().to_string() << std::endl;
+        }).wait();
+
+        std::cout << "Press Enter to exit." << std::endl;
+        std::string line;
+        std::getline(std::cin, line);
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    listener.close().wait();
 
     /* Check argument count */
     if (argc != 6)
